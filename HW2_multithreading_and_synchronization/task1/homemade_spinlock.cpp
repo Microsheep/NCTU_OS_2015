1adb5cd451d52ae54a65ab5ebfdd9d464a06e445
microsheep <twmicrosheep@gmail.com>
2015-11-21 20:28:44 +0800

    task1 finish

---
+++ HW2_multithreading_and_synchronization/task1/homemade_spinlock.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+
+class Counter{
+private:
+    int value;
+public:
+    Counter(){
+        value=0;
+    }
+    void Increment(){
+        value++;
+    }
+    void Print(){
+        cout << value << endl;
+    }
+};
+
+Counter x;
+int home_spin=0;
+
+void homemade_spin_lock(int* spinlock_addr){
+    asm(
+        "spin_lock: \n\t"
+        "xorl %%ecx, %%ecx \n\t"
+        "incl %%ecx \n\t"
+        "spin_lock_retry: \n\t"
+        "xorl %%eax, %%eax \n\t"
+        "lock; cmpxchgl %%ecx, (%0) \n\t"
+        "jnz spin_lock_retry \n\t"
+        ::"r" (spinlock_addr):"ecx","eax"
+    );
+}
+
+void homemade_spin_unlock(int* spinlock_addr){
+    asm(
+        "spin_unlock: \n\t"
+        "movl $0, (%0) \n\t"
+        ::"r" (spinlock_addr):
+    );
+}
+
+void* ThreadRunner(void*){
+    int k;
+    for (k=0;k<100000000;k++){
+        homemade_spin_lock(&home_spin);
+        x.Increment();
+        homemade_spin_unlock(&home_spin);
+    }
+}
+
+int main(){
+    pthread_t tid[3];
+    int i;
+    home_spin=0;
+    for(i=0;i<3;i++){
+        pthread_create(&tid[i], NULL, ThreadRunner, 0);
+    }
+    for(i=0;i<3;i++){
+        pthread_join(tid[i], NULL);
+    }
+    x.Print();
+    return 0;
+}
+
